996778a6332fc778ca5e392f74d2670d825ab178
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2023-04-04 19:14:05 -0400

    Add files via upload

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -16,4 +16,63 @@ export default function createNFT() {
   });
   const router = useRouter();
   const [loadingState, setLoadingState] = useState('not-loading');
+
+  async function imageUpload(e) {
+    const file = e.target.files[0];
+
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+      const resFile = await axios({
+        method: 'post',
+        url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
+        data: formData,
+        headers: {
+          pinata_api_key: PINATA_KEY,
+          pinata_secret_api_key: PINATA_SECRET,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      const ImageURL = `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`;
+      setFileUrl(ImageURL);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  //File upload metadata to IPFS and then return URL to use in later transaction
+  async function uploadToIPFS() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) return;
+    setLoadingState('loading');
+
+    try {
+      var jsondata = JSON.stringify({
+        pinataMetadata: {
+          name: `${name}.json`,
+        },
+        pinataContent: {
+          name,
+          description,
+          image: fileUrl,
+        },
+      });
+
+      const resFile = await axios({
+        method: 'post',
+        url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+        data: jsondata,
+        headers: {
+          pinata_api_key: PINATA_KEY,
+          pinata_secret_api_key: PINATA_SECRET,
+          'Content-Type': 'application/json',
+        },
+      });
+      const tokenURI = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+      return tokenURI;
+    } catch (error) {
+      console.log('Error uploading file: ', error);
+    }
+  }
 }
