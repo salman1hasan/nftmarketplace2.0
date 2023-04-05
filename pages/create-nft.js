ec9a57c2acab74a91c512c8af1f47897c324754d
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2023-04-05 03:47:34 -0400

    Add files via upload

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -1,4 +1,4 @@
-import { userState } from 'react';
+import { useState } from 'react';
 import { ethers } from 'ethers';
 import { useRouter } from 'next/router';
 import Web3Modal from 'web3modal';
@@ -8,7 +8,7 @@ import axios from 'axios';
 import Image from 'next/image';
 
 export default function createNFT() {
-  const [fileUri, setFileUrl] = useState(null);
+  const [fileUrl, setFileUrl] = useState(null);
   const [formInput, updateFormInput] = useState({
     price: '',
     name: '',
@@ -75,4 +75,97 @@ export default function createNFT() {
       console.log('Error uploading file: ', error);
     }
   }
+
+  async function listNFTForSale() {
+    const url = await uploadToIPFS();
+    const web3modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const getnetwork = await provider.getNetwork();
+    const goerliChainId = 5;
+    if (getnetwork.chainId != goerliChainId) {
+      alert('You are not connected to Goerli network');
+      return;
+    }
+
+    //sign the transaction
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress,
+      NFTMarketplace.abi,
+      signer
+    );
+    const price = ethers.utils.parseUnits(formInput.price, 'ether');
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
+    let transaction = await contract.createToken(url, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
+    router.push('/');
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/8 flex-col mr-10 mt-10">
+        {!fileUrl && (
+          <Image
+            className="rounded mt-4"
+            src="/placeholder.png"
+            width={300}
+            height={200}
+          />
+        )}
+        {fileUrl && (
+          <Image
+            src={fileUrl}
+            alt="Image uploaded succesfully"
+            width={300}
+            height={200}
+            placeholder="blur"
+            blurDataURL="/placeholder.png"
+          />
+        )}
+      </div>
+      <div className="w-1/2 flex flex-col">
+        <input
+          placeholder="Asset Name"
+          className="mt-8 border rounded p=4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, name: e.target.value })
+          }
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, description: e.target.value })
+          }
+        />
+        <input
+          placeholder="Asset Price in eth"
+          className="mt-2 border rounded p-4"
+          type="number"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, price: e.target.value })
+          }
+        />
+        <input
+          type="file"
+          name="Asset"
+          className="my-4"
+          onChange={imageUpload}
+        />
+
+        {fileUrl && (
+          <button
+            onClick={listNFTForSale}
+            className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
+          >
+            {loadingState == 'not-loading' ? 'Create NFT' : 'Wait uploading...'}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
